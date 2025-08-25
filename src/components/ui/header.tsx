d82a702c82d3229fcf5a977b1a2b02554d194e75
sanjitxdutta <duttasanjit691@gmail.com>
2025-08-25 22:14:30 +0530

    Setup the header

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,50 @@
+import { useTheme } from '@/context/theme-provider'
+import { Moon, Sun } from 'lucide-react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+type Props = {}
+
+const Header = (props: Props) => {
+
+    const { theme, setTheme } = useTheme();
+    const isDark = theme === "dark";
+
+    return <header className='sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur py-2 supports-[backdrop-filter]:bg-background/60'>
+        <div className='container mx-auto flex h-16 items-center justify-between px-4'>
+            <Link to={"/"}>
+                <div className="flex items-center">
+                    <img
+                        src={isDark ? "/logo.png" : "/logo2.png"}
+                        alt='Nimbus Logo'
+                        className='h-14'
+                    />
+                    <span
+                        className={`ml-2 flex items-center font-extrabold tracking-wide ${isDark ? "text-white" : "text-gray-900"
+                            } text-3xl md:text-4xl`}
+                    >
+                        Nimbus
+                    </span>
+
+                </div>
+            </Link>
+
+            <div>
+                <div
+                    onClick={() => setTheme(isDark ? "light" : "dark")}
+                    className={`flex items-center cursor-pointer transition-transform duration-500 ${isDark ? "rotate-180" : "rotate-0"
+                        }`}
+                >
+                    {isDark ? (
+                        <Sun className='h-6 w-6 text-yellow-500 rotate-0 transition-all' />
+                    ) : (
+                        <Moon className='h-6 w-6 text-blue-500 rotate-0 transition-all' />
+                    )}
+                </div>
+            </div>
+        </div>
+
+    </header>
+}
+
+export default Header
