9d47b0d0e13c314a5000cd60ff2e29e0dfe87259
sanjitxdutta <duttasanjit691@gmail.com>
2025-08-27 08:57:15 +0530

    Integrated geo-location tracking

--- src/pages/weather-dashboard.tsx
+++ src/pages/weather-dashboard.tsx
@@ -1,8 +1,36 @@
+import { Button } from '@/components/ui/button'
+import { useGeolocation } from '@/hooks/use-geolocation'
+import { RefreshCcw } from 'lucide-react'
 import React from 'react'
 
 const WeatherDashboard = () => {
+  const { coordinates, error: locationError, getLocation, isLoading } = useGeolocation();
+
+  console.log(coordinates);
+
+  const handelRefreash = () => {
+    getLocation();
+    if(coordinates){
+      //  reload weather data
+    }
+  };
+
   return (
-    <div>WeatherDashboard</div>
+    <div>
+      {/* Favourite Cities */}
+      <div className='flex items-center justify-between'>
+        <h1 className='text-xl font-bold tracking-tight'>My Location</h1>
+        <Button variant={'outline'}
+          size={"icon"}
+          onClick={handelRefreash}
+        //</div>disabled={}
+        >
+          <RefreshCcw className='h*4 w-4' />
+        </Button>
+      </div>
+
+      {/* current and hourly weather */}
+    </div>
   )
 }
 
